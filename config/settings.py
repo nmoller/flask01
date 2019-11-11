41c846890b511ddc1aeb7911c7176d3e8830c1d5
Nelson Moller <nmoller.c@gmail.com>
2019-11-11 10:16:18 -0500

    Connexion, query DB et format tables:
    En modifiant les paramètres, la connexion à la BD fonctionne.
    Ajout de datatables à l'affichages des utilisateurs de mysql.
    Test only.

--- config/settings.py
+++ config/settings.py
@@ -3,9 +3,9 @@ DEBUG = True
 SECRET_KEY='No-so-secret-key'
 
 
-MYSQL_DATABASE_HOST	= 'mysqldb'
-MYSQL_DATABASE_PORT	= 3306
-MYSQL_DATABASE_USER	= 'inscription'
-MYSQL_DATABASE_PASSWORD	= 'root'
-MYSQL_DATABASE_DB	= 'inscription'
-MYSQL_DATABASE_CHARSET = 'utf-8'
+MYSQL_HOST	= 'mysqldb'
+MYSQL_PORT	= 3306
+MYSQL_USER	= 'root'
+MYSQL_PASSWORD	= 'root'
+MYSQL_DB	= 'inscription'
+MYSQL_CURSORCLASS = 'DictCursor'
--- inscription/blueprints/page/views.py
+++ inscription/blueprints/page/views.py
@@ -1,8 +1,17 @@
 from flask import Blueprint, render_template
+from flask import current_app
+from inscription.extensions import db
+
 
 page = Blueprint('page', __name__, template_folder='templates')
 
 
+
 @page.route('/')
 def home():
-    return render_template('page/home.html')
+  cur = db.connection.cursor()
+  cur.execute('''SELECT user, host FROM mysql.user''')
+  rv = cur.fetchall()
+  result = rv
+  #return str(rv)
+  return render_template('page/home.html', result = result)
